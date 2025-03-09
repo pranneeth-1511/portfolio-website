b90f8d1b6eb77fe7bcb23355a1a74dc09c6b7e78
D.K.Pranneeth <118660814+pranneeth-1511@users.noreply.github.com>
2025-03-09 15:12:00 +0530

    Project link add

--- src/Pages/ProjectsSection/ProjectsSection.js
+++ src/Pages/ProjectsSection/ProjectsSection.js
@@ -8,17 +8,17 @@ const ProjectsSection = () => {
         <h2 className="section-title">Projects</h2>
         <div className="projects-list">
           <div className="project-item">
-            <h3 className="sub-title">E-Commerce platform without intermediaries</h3>
+            <h3 className="project-title">E-Commerce platform without intermediaries</h3>
             <p className="project-item-description">Developed a decentralized e-commerce platform enabling direct transactions between buyers and sellers, 
               eliminating the need for intermediaries. Focused on reducing transaction costs and enhancing transparency.</p>
           </div>
           <div className="project-item">
-            <h3 className="sub-title">Online Voting System</h3>
+            <h3 className="project-title">Online Voting System</h3>
             <p className="project-item-description">Designed and implemented a secure and user-friendly online voting platform to streamline election processes, 
               ensuring transparency, accessibility, and efficiency.</p>
           </div>
           <div className="project-item">
-            <h3 className="sub-title">Smart wheelchair</h3>
+            <a href="https://github.com/pranneeth-1511/Smart-Wheel-Chair" target="_blank" rel="noopener noreferrer" className="link-project-title">Smart wheelchair</a>
             <p className="project-item-description">Designed an IoT-enabled wheelchair using Python and Tkinter, joystick, and mobile app control.
               Features include obstacle detection and health monitoring, enhancing accessibility and safety</p>
           </div>
